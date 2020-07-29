58a3421ca010fc5775b115a6b38669c7620e8ecb
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-07-29 07:29:23 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,11 @@
+import numpy as np
+import numbers
+print("lets do neural networking" )
+input_matrix = np.array([2,4,1])
+weights_of_matrices = np.array([4,3,5])
+x = np.array([1,1,1])
+for val in input_matrix:
+    x = val * weights_of_matrices
+output_dot_product = input_matrix * weights_of_matrices
+print(output_dot_product)
+print(x)    
