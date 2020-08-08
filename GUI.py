b88482126234ac353923e6e36a7845b5fb1fa988
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-08-08 09:38:45 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -8,3 +8,23 @@ def buttonRespond():
 b = tkinter.Button(text = "hello" , command  = buttonRespond)
 b.pack()
 gui.mainloop()
+
+
+
+
+
+
+'''canvas '''
+import tkinter
+from tkinter import messagebox
+gui = tkinter.Tk()
+def buttonRespond():
+    messagebox.showinfo("message","Hello user\ncanvas created")
+    w = tkinter.Canvas(bg = "white",height = 250, width = 300)
+    coord = 10,50,200,210
+    arc = w.create_arc(coord, start = 0, extent = 180, fill = "green")
+    w.pack()
+    arc.pack()
+b = tkinter.Button(text = "hello" ,activebackground = "red", font = "arial", command = buttonRespond)
+b.pack()
+gui.mainloop()
