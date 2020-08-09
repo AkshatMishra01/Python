85239ece7f952698b4546fc9c94e14c80a17072e
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-08-09 19:41:50 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -28,3 +28,55 @@ def buttonRespond():
 b = tkinter.Button(text = "hello" ,activebackground = "red", font = "arial", command = buttonRespond)
 b.pack()
 gui.mainloop()
+
+
+
+
+
+#new one
+import tkinter 
+from tkinter import messagebox
+gui2 = tkinter.Tk()
+frame = tkinter.Frame(gui2)
+frame.pack()
+command1 = bool
+command2 = bool
+var1 = tkinter.IntVar()
+var2 = tkinter.IntVar()
+l = tkinter.Label(text = 'User Name')
+e = tkinter.Entry(bd = 4,text ='left')
+def function():
+    messagebox.showinfo('GUI','select any one of the following')
+b = tkinter.Button(text = "click me !", command = function)
+
+def response():
+    if command1 == True:
+        messagebox.showinfo('please select a video from playlist')
+    elif command2 == True:
+        messagebox.showinfo('please select a song from playlist')
+
+def video():
+    messagebox.showinfo('GUI',"you selected video")
+    c2.deselect()
+    response()
+    command1 = True
+    return command1
+def music():
+    messagebox.showinfo('GUI','you selected music')
+    c.deselect()
+    command2 = True
+    return command2
+c = tkinter.Checkbutton(text = 'video',variable = var1,height = 5, width = 20,command = video)
+c2 = tkinter.Checkbutton(text = 'music',variable = var2,height = 5, width = 20, command = music)
+w1 = tkinter.Listbox()
+w1.insert(1,'gui for access management')
+w1.insert(2,'gui for seasonal projects')
+w1.insert(3,'gui for selfmade antivirus')
+w1.insert(4, ' website for gui construction')
+w1.pack(side = 'top')
+l.pack(side = 'left')
+e.pack(side = 'left')
+c.pack()
+c2.pack()
+b.pack(side = 'left')
+
