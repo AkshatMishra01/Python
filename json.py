9b3f8e969f218ca9618f0863c83fb1655fd4d120
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-15 09:40:35 +0530

    json in python

---
+++ json.py
@@ -0,0 +1,7 @@
+JSON is an open standard for exchanging data on the web.
+It supports data structures like object and array. 
+So it is easy to write and read data from JSON. 
+JSON stands for JavaScript Object Notation. 
+JSON is an open standard data-interchange format. 
+JSON is lightweight and self describing.
+JSON is originated from JavaScript.
