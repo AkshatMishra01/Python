a0095b8448eecd5db03ad40157971758b0768e18
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-15 09:57:01 +0530

    exception handling in python

--- try.py
+++ try.py
@@ -4,3 +4,11 @@ except:
   print("Something went wrong")
 else:
   print("Nothing went wrong")
+
+  #finally keyword in python 
+  try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
