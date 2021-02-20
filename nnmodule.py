07a890c5f7c38a478ab034e217df7f58315bee06
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-20 20:12:15 +0530

    My own self-created module in python for a very special purpose

---
+++ nnmodule.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mat
+
+print("Running Neural Network.....")
+def import_data():
+    pd.open("training data.csv")
+    pd.DataFrame("training data.csv")
+
+import_data()
