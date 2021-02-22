13a051afa93059fe8f637fda64f0c3e9a9a4cde1
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-22 14:26:24 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,9 @@
+age = 18
+if(age < 18):
+    print("Go see the meat loaf")
+elif (age == 18):
+    print("I'll let u pass")
+else:
+    print("You can definitely enter")
+
+print("OK, Please move on!!")
