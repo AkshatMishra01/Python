c4f1a04e976e06947ccfa18aa8a29294e51eb658
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-06-03 08:10:24 +0530

    Update ar.py

--- ar.py
+++ ar.py
@@ -1,11 +1,18 @@
 import numpy as np
 import numbers
 print("lets do neural networking" )
-input_matrix = np.array([2,4,1])
-weights_of_matrices = np.array([4,3,5])
+input_matrix = np.array([2,4,1,7,8])
+weights_of_matrices = np.array([4,3,5,6,9])
 x = np.array([1,1,1])
 for val in input_matrix:
     x = val * weights_of_matrices
 output_dot_product = input_matrix * weights_of_matrices
 print(output_dot_product)
 print(x)    
+
+
+
+
+
+
+
